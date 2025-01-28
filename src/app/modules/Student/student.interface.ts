af0350e22196958228662b5e022d28c738b1c3fe
HelloKowshik <hellokowshik@gmail.com>
2025-01-29 02:17:57 +0600

    basic schema added

---
+++ src/app/modules/Student/student.interface.ts
@@ -0,0 +1,49 @@
+export type TGender = "Male" | "Female";
+export type TBloodGroup =
+  | "A+"
+  | "A-"
+  | "B+"
+  | "B-"
+  | "O-"
+  | "O+"
+  | "AB+"
+  | "AB-";
+
+export type TName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation?: string;
+  motherContactNo?: string;
+};
+
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type TStudent = {
+  id: string;
+  name: TName;
+  email: string;
+  gender: TGender;
+  dateOfBirth?: string;
+  contactNo: string;
+  emergenceyContactNo: string;
+  bloodGroup?: TBloodGroup;
+  presentAddress: string;
+  permanentAddress: string;
+  guardianInfo: TGuardian;
+  localGuardian: TLocalGuardian;
+  profileImg?: string;
+  isActive: "active" | "blocked";
+};
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import mongoose from "mongoose";
+import config from "./app/config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    app.listen(config.port, () => {
+      console.log(`Server is running on PORT:${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
